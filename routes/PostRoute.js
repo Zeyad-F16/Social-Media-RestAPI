342a699e4707686da254d9c9777606d49595ff67
Zeyad <farghlyzeyad9@gmail.com>
2024-07-01 10:31:30 +0300

    post functions

--- routes/PostRoute.js
+++ routes/PostRoute.js
@@ -1,8 +1,29 @@
 const express  = require('express');
 const router = express.Router();
-const {createPostController}= require('../controllers/PostController');
+const {createPostController ,
+ createPostWithImageController ,
+  updatePostController ,
+  getUserPostsController,
+  getallPostsController,
+  deletePostController}= require('../controllers/PostController');
+const uploadImage= require('../middlewares/uploadImageMiddleware');    
 
 // Create post
 router.post("/create", createPostController );
+
+// Create Post With image
+router.post('/create/:userId', uploadImage.array("images",5) , createPostWithImageController);
+
+// update post
+router.put("/update/:postId", updatePostController);
+
+// get all posts
+router.get("/all/:userId", getallPostsController);
+
+// get user posts
+router.get("/user/:userId", getUserPostsController);
+
+// delete post
+router.delete("/delete/:postId", deletePostController);
 
 module.exports = router;
