f1fa7504481342923063f20c73c7b21af13ea444
Zeyad <farghlyzeyad9@gmail.com>
2024-07-10 19:44:21 +0300

    create and update reply comment

--- controllers/CommentController.js
+++ controllers/CommentController.js
@@ -11,7 +11,7 @@ exports.createCommentController = async (req, res, next) => {
         throw new CustomError("User not found",404);  
     }
     const post = await PostDB.findById(postId);
-    if(!user){
+    if(!post){
         throw new CustomError("post not found",404);  
     }
     
@@ -25,10 +25,91 @@ exports.createCommentController = async (req, res, next) => {
     post.comments.push(newComment._id);
     await post.save();
     
-    res.status(201).json({message:"post Created Successfully", comment:newComment});
+    res.status(201).json({message:"Comment Created Successfully", comment:newComment});
     }
     
     catch(error) {
      next(error);
+    }
+}
+
+exports.updateCommentController=async (req,res,next)=>{
+
+    const {commentId}=req.params;
+    const {text}=req.body;
+
+    try{
+        const commentToUpdate = await CommentDB.findById(commentId)
+        if(!commentToUpdate){
+            throw new CustomError("Comment not found!",404);
+        }
+
+        const updatedComment = await CommentDB.findByIdAndUpdate(commentId, {text} , {new:true} );
+
+        res.status(200).json({message:"Comment updated successfully!",updatedComment});
+
+    }
+    catch(error){
+        next(error);
+    }
+}
+
+exports.createCommentReplyController = async(req, res , next)=>{
+    const {userId,text} = req.body;
+    const {commentId} = req.params;
+    
+    try{
+        const user = await UserDB.findById(userId);
+        const comment = await CommentDB.findById(commentId);
+        if(!user){
+            throw new CustomError("User Not Found!",404);
+        }
+        if(!comment){
+            throw new CustomError("Comment Not Found!",404);
+        }
+        
+        const reply = {
+            text,
+            user:userId,
+        }
+        
+        comment.replies.push(reply);
+        await comment.save();
+        res.status(200).json({message:"Reply Created Successfully",reply});
+        
+    }
+    catch(error){
+        next(error);
+    }
+}
+
+
+exports.updateReplyCommentController=async(req,res,next)=>{
+
+    const {commentId,replyId}=req.params;
+    const {text,userId}=req.body;
+    try{
+        const comment=await CommentDB.findById(commentId)
+        if(!comment){
+            throw new CustomError("Comment not found!",404);
+        }
+
+        const replyIndex=comment.replies.findIndex((reply)=>reply._id.toString()===replyId)
+        if(replyIndex===-1){
+            throw new CustomError("Reply not found!",404);
+        }
+
+        if(comment.replies[replyIndex].user.toString()!==userId){
+            throw new CustomError("You can only update your comments",404);
+        }
+
+        comment.replies[replyIndex].text=text;
+
+        await comment.save();
+        res.status(200).json({message:"Reply updated successfully!",comment});
+
+    }
+    catch(error){
+        next(error);
     }
 }
--- routes/CommentRoute.js
+++ routes/CommentRoute.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const { createCommentController }=require('../controllers/CommentController');
+const { createCommentController ,createCommentReplyController ,updateCommentController, updateReplyCommentController }=require('../controllers/CommentController');
 
 router.post('/create',createCommentController);
+router.post('/create/reply/:commentId',createCommentReplyController);
+router.put('/updateComment/:commentId',updateCommentController);
+router.put('/update/:commentId/reply/:replyId',updateReplyCommentController);
 
 module.exports= router;
