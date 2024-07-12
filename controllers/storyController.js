225a6909381ba90cd6aec51ee1e8514ee3ca9169
Zeyad <farghlyzeyad9@gmail.com>
2024-07-12 19:33:45 +0300

    get stories

--- controllers/storyController.js
+++ controllers/storyController.js
@@ -35,3 +35,24 @@ exports.createStoryController=async(req,res,next)=>{
         next(error);
     }
 }
+
+
+exports.getStoriesController=async(req,res,next)=>{
+    const {userId}=req.params;
+    try{
+        const user=await UserDB.findById(userId);
+        
+        if(!user){
+            throw new CustomError("No user found",404);
+        }
+
+        const followingUsers=user.following;
+        const stories=await StoryDB.find({user:{$in:followingUsers}}).populate("user","fullName username profilePicture");
+
+        res.status(200).json(stories);
+
+    }
+    catch(error){
+        next(error);
+    }
+}
--- routes/storyRoute.js
+++ routes/storyRoute.js
@@ -2,10 +2,14 @@ const express=require("express");
 
 const router=express.Router();
 
-const { createStoryController } = require('../controllers/storyController');
+const { createStoryController,
+        getStoriesController
+ } = require('../controllers/storyController');
 const upload = require('../middlewares/uploadImageMiddleware');
 
 router.post('/create/:userId' ,upload.single("image") ,  createStoryController);
+
+router.get('/all/:userId' , getStoriesController);
 
 
 module.exports=router;
